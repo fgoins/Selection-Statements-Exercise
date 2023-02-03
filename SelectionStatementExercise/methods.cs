50c7d12bb03f6a1ee853307bbe7e9ae4bb464401
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-02-03 13:56:58 -0600

    corrected ssh link

---
+++ SelectionStatementExercise/methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatementExercise
+{
+    internal class methods
+    {
+        public static void Game(string[] args) 
+        {
+            var r = new Random();
+            var favnumber = r.Next(1, 20);
+
+            Console.WriteLine("pick a number between 1 and 1000);
+            var userinput = int.Parse(Console.ReadLine());
+
+            if (userinput > favnumber)
+            {
+                Console.WriteLine($"{userinput} too low");
+            }
+            else if (userinput < favnumber)
+            {
+                Console.WriteLine($"{userinput} to high");
+            }
+            else 
+            {
+                Console.WriteLine("You Guessed it!!!");
+            }
+        }
+
+
+    }
+}
